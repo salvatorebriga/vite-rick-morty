73aa10712adf9c3e7f427f35954dc14bed18a4c2
salvatorebriga <salvo.z2302@gmail.com>
2024-06-03 20:01:08 +0200

    fixed nav

--- src/components/nav/AppNav.vue
+++ src/components/nav/AppNav.vue
@@ -16,11 +16,13 @@
 <style lang="scss" scoped>
   div {
     padding-top: 50px;
+    padding-left: 10px;
+    padding-right: 10px;
     display: flex;
+    flex-wrap: wrap;
     gap: 30px;
     justify-content: center;
     align-items: center;
-
     input,
     select,
     .search,
@@ -34,14 +36,12 @@
         cursor: pointer;
       }
     }
-
     .search {
       background-color: rgb(253, 182, 120);
       &:hover {
         background-color: rgb(251, 151, 63);
       }
     }
-
     .reset {
       background-color: rgb(121, 130, 253);
       &:hover {
