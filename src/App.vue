1bab2daa0e71bab42c0a2f3e38081699b75dc21f
salvatorebriga <salvo.z2302@gmail.com>
2024-06-03 19:59:08 +0200

    added AppNav content

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <script>
   import AppHeader from "./components/header/AppHeader.vue";
+  import AppNav from "./components/nav/AppNav.vue";
   export default {
     components: {
       AppHeader,
+      AppNav,
     },
   };
 </script>
 
 <template>
   <AppHeader />
+  <AppNav />
 </template>
 
 <style lang="scss" scoped></style>
---
+++ src/components/nav/AppNav.vue
@@ -0,0 +1,52 @@
+<script>
+  export default {};
+</script>
+
+<template>
+  <div>
+    <input type="text" placeholder="Search character" />
+    <select name="status" id="status">
+      <option value="default">Select status</option>
+    </select>
+    <button class="search" value="Search">Search</button>
+    <button class="reset" value="Reset">Reset</button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  div {
+    padding-top: 50px;
+    display: flex;
+    gap: 30px;
+    justify-content: center;
+    align-items: center;
+
+    input,
+    select,
+    .search,
+    .reset {
+      padding: 10px;
+      border-radius: 5px;
+      border: none;
+      font-size: 20px;
+
+      &:hover {
+        cursor: pointer;
+      }
+    }
+
+    .search {
+      background-color: rgb(253, 182, 120);
+      &:hover {
+        background-color: rgb(251, 151, 63);
+      }
+    }
+
+    .reset {
+      background-color: rgb(121, 130, 253);
+      &:hover {
+        background-color: rgb(95, 106, 252);
+      }
+    }
+  }
+</style>
